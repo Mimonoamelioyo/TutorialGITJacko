ba7a262e0ab18a9df7f4b602375973108dcfcb2f
javfe <javfe@Javier>
2017-03-22 11:36:48 +0100

    Agregado comentario

--- TutorialGITJacko/src/tGITJacko/HolaMundo.java
+++ TutorialGITJacko/src/tGITJacko/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Comentario agregado desde un cliente
 		System.out.println("Hola Mundo by Jacko");
 
 	}
