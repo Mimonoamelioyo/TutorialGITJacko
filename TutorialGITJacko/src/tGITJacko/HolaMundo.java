60a11859b7b935a294cadb56da94646c0019f1c6
Entornos <Entornos@Entornos-PC.localdomain>
2017-03-22 12:07:34 +0100

    Cambiada clase HolaMundo con un ciclo

--- TutorialGITJacko/src/tGITJacko/HolaMundo.java
+++ TutorialGITJacko/src/tGITJacko/HolaMundo.java
@@ -1,11 +1,19 @@
 package tGITJacko;
 
 public class HolaMundo {
+	String nombre = "Jacko";
+	int numAle = 1;
+	static int datos[] = {1,2,3};
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Comentario agregado desde un cliente
 		System.out.println("Hola Mundo by Jacko");
+		System.out.println("Numeros: ");
+		for(int i:datos){
+			System.out.println("Numero siguiente: " + datos[i]);
+		}
 
 	}
 
